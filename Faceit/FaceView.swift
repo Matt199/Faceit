342538102f0aa4abcdf092cc2af08f7b5817a45f
Matt <Matt@macbook-air-magda.local>
2016-09-08 10:15:25 +0200

    New Cocoa Touch and changed ViewController to FaceViewController

---
+++ Faceit/FaceView.swift
@@ -0,0 +1,21 @@
+//
+//  FaceView.swift
+//  Faceit
+//
+//  Created by Matt on 08.09.2016.
+//  Copyright © 2016 Matt. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
